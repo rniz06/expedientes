f4d8223454f0cff32d540cb14904a07b8596da91
Ronald Niz <ronaldalexisniznunez@gmail.com>
2025-01-19 18:14:03 -0300

    Se modifica el generador de N° mesa de entrada de 00001 a 0001

--- app/Services/Expediente/NroMesaEntradaGenerador.php
+++ app/Services/Expediente/NroMesaEntradaGenerador.php
@@ -31,10 +31,10 @@ class NroMesaEntradaGenerador
             // Extraer el número después del año
             $partes = explode('-', $ultimoExpediente->mesa_entrada_completa);
             $numeroActual = end($partes); // Tomar la última parte (el número)
-            $nuevoNumero = str_pad((int)$numeroActual + 1, 5, '0', STR_PAD_LEFT); // Incrementar y rellenar con ceros
+            $nuevoNumero = str_pad((int)$numeroActual + 1, 4, '0', STR_PAD_LEFT); // Incrementar y rellenar con ceros
         } else {
             // Si no hay registros para el año actual, empezar desde el 1
-            $nuevoNumero = "00001";
+            $nuevoNumero = "0001";
         }
 
         // Devolver el nuevo número en el formato requerido
